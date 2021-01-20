1b3c242347e8bfea94c8b0894c6221fb1dc20b6d
Lurzapps <lurzapps@gmail.com>
2021-01-20 11:46:03 +0100

    update

---
+++ text_helper.py
@@ -0,0 +1,87 @@
+import re
+import emoji
+
+
+def word_count(sent):
+    return len(sent.split())
+
+
+def remove_urls(sent):
+    return re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', sent)
+
+
+def emoji_count(sent):
+    e_sent = emoji.demojize(sent)
+    return len(re.findall(r':(.*?):', e_sent))
+
+
+def emoji_to_text(sent):
+    e_sent = emoji.demojize(sent)
+    emo = re.findall(r':(.*?):', e_sent)
+    for e in emo:
+        e_sent = e_sent.replace(':{}:'.format(e), '{}'.format(e))
+    return e_sent
+
+
+def count_hashtags(text):
+    get_hashtags = re.findall(r'#\w*[a-zA-Z]\w*', text)
+    return len(get_hashtags)
+
+
+def remove_hashtags(text):
+    return re.sub(r'#\w*[a-zA-Z]\w*', '', text)
+
+
+def extract_username(sent):
+    usernames = re.findall(r'@[A-Za-z0-9_$]*', sent)
+    return usernames
+
+
+def replace_username(sent):
+    usernames = extract_username(sent)
+    for un in usernames:
+        un = re.sub('@', '', un)
+        sent = sent.replace('@{}'.format(un), '{}'.format(un))
+    return sent
+
+
+def remove_number(text):
+    return re.sub(r'#[0-9]+', '', text)
+
+
+def find_punctuations(text):
+    return re.findall(r'[.?"\'`,\-!:;()\[\]\\/“”]+?', text)
+
+
+def count_punctuations(text):
+    return len(re.findall(r'[.?"\'`,\-!:;()\[\]\\/“”]+?', text))
+
+
+def remove_punctuations(text):
+    # keep points for splitting
+    return re.sub(r'[.?"\'`,\-!:;()\[\]\\/“”]+?', '.', text)
+
+
+def remove_symbols(text):
+    # only keep alphabet
+    new = ''
+    for letter in text:
+        for c in 'abcdefghijklmnopqrstuvwxyz ':
+            if letter == c:
+                new += c
+                break
+
+    return re.sub(r' +', ' ', new)
+
+
+def uppercase_letters(text):
+    upper = 0
+    for letter in text:
+        if letter.isupper():
+            upper += 1
+    return upper
+
+
+def transform(text) -> str:
+    return remove_symbols(remove_urls(
+        remove_hashtags(replace_username(remove_number(remove_punctuations(str.lower(emoji_to_text(str(text)))))))))
--- train_model.py
+++ train_model.py
@@ -1,36 +1,31 @@
 from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import CountVectorizer
+from scipy.sparse import csr_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import pickle as pkl
+import numpy as np
 
 from sklearn.linear_model import LogisticRegression
 
-df = pd.read_csv('datasets/train2.csv', sep=';')
+df = pd.read_csv('datasets/train.csv', sep=';')
 
 stop = list(stopwords.words('english'))
 
-train, valid = train_test_split(df, test_size=0.2, random_state=0, stratify=df.target.values)
+vectorizer = CountVectorizer(decode_error='replace', stop_words=stop, ngram_range=(1, 2))
 
-vectorizer = CountVectorizer(decode_error='replace', stop_words=stop)
-
-X_train = vectorizer.fit_transform(train['text'].values.astype('U'))
-X_valid = vectorizer.transform(valid['text'].values.astype('U'))
-
-y_train = train.target.values
-y_valid = valid.target.values
+X_train = vectorizer.fit_transform(df['text'].values.astype('U'))
+y_train = df.target.values
 
 print("X_train.shape : ", X_train.shape)
-print("X_train.shape : ", X_valid.shape)
 print("y_train.shape : ", y_train.shape)
-print("y_valid.shape : ", y_valid.shape)
 
 model = LogisticRegression(max_iter=300)
 model.fit(X_train, y_train)
 
-print(model.score(X_train, y_train))
+with open('models/vectorizer.pkl', 'wb') as f:
+    pkl.dump(vectorizer, f)
 
-# todo: test other models, implement text features (extracted), save the model and vectorizer and predict for own
-# todo: port to app or desktop usage / tensorflow model etc.
-
-
+with open('models/model.pkl', 'wb') as f:
+    pkl.dump(model, f)
