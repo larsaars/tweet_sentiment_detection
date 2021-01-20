734e69bcca33819d745b453674133354a1f64214
Lurzapps <lurzapps@gmail.com>
2021-01-21 00:20:23 +0100

    fit model without calculating score; somehow the score is always zero, i tried everything but could not find the cause

--- train_model.py
+++ train_model.py
@@ -1,64 +1,45 @@
 from nltk.corpus import stopwords
-from scipy.sparse import csr_matrix
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn.model_selection import train_test_split, KFold
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import pickle as pkl
 import numpy as np
-import text_helper as txth
 
 from sklearn.linear_model import LogisticRegression
 
 # read the csv with all data
-df = pd.read_csv('datasets/all/tweets_train.csv', sep=';')
-# make sure text is string
+df = pd.read_csv('datasets/all/inida_covid_tweets.csv', sep=';')
+# ensure text is string
 df['text'] = df.text.apply(str)
+df['target'] = df.target.apply(str)
 # init list of stopwords
 stop = list(stopwords.words('english'))
+# split with hold - out validation - just to calc a score,
+# after that the whole model will be recalculated
+# train_split, validation_split = train_test_split(df, test_size=0.2, random_state=0, stratify=df.target.values)
 # count-vectorize words in the given ngram_range
 vectorizer = CountVectorizer(decode_error='replace', stop_words=stop, ngram_range=(1, 2))
 # fit the vectorizer (etc. for later whole model training)
-X_train = vectorizer.fit_transform(df.text.values.astype('U'))
-y_train = df.target
-# index of the loop and list of scores to calc mean
-idx = 0
-# init a 5 split k fold for cross validation
-for train_df_idx, test_df_idx in KFold(n_splits=5, shuffle=True).split(df):
-    # create dataframes from split indices
-    train_df = pd.DataFrame(df.iloc[train_df_idx])
-    test_df = pd.DataFrame(df.iloc[test_df_idx])
-
-    train_df['text'] = train_df.text.apply(txth.transform)
-    test_df['text'] = test_df.text.apply(txth.transform)
-
-    # gather target and training data from data
-    training_data = vectorizer.transform(train_df.text.values.astype('U'))
-    training_target = train_df.target.values
-
-    # same for for testing split set
-    testing_data = vectorizer.transform(test_df.text.values.astype('U'))
-    testing_target = test_df.target.values
-
-    # calc for each split a score by:
-    # 1. create model
-    model = LogisticRegression(max_iter=300)
-    # 2. fit model
-    model.fit(training_data, training_target)
-    # 3. print score
-    score = model.score(testing_data, testing_target)
-    scores.append(score)
-    print('%i. %d' % (idx, score))
-    # idx++
-    idx += 1
-
-# print mean score
-print('kfold mean: %d' % np.mean(scores))
+final_X_train = vectorizer.fit_transform(df.text.values.astype('U'))
+final_y_train = df.target
+# # for score calculation
+# score_X_train = vectorizer.transform(train_split['text'].values.astype('U'))
+# score_X_valid = vectorizer.transform(validation_split['text'].values.astype('U'))
+# score_y_train = train_split.target.values
+# score_y_valid = validation_split.target.values
+# # calc test score
+# # 1. create model
+# model = LogisticRegression(max_iter=300)
+# # 2. fit model
+# model.fit(score_X_train, score_y_train)
+# # 3. print score
+# print('score: %d' % model.score(score_X_valid, score_y_valid))
 
 # after predicting score train everything and save to binaries
 model = LogisticRegression(max_iter=300)
-model.fit(X_train, y_train)
+model.fit(final_X_train, final_y_train)
 
+# and dump into files
 with open('models/vectorizer.pkl', 'wb') as f:
     pkl.dump(vectorizer, f)
 
